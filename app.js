190cd1dfdb6c3d0168240a694f83f63ae2b97e30
mujyo <tokugawaieneko@gmail.com>
2023-04-12 18:43:25 -0700

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ import TuitsController
   from "./controllers/tuits/tuits-controller.js";
 import mongoose from "mongoose"; 
 
-const CONNECTION_STRING = 'mongodb+srv://mengruwangcolab:12345abcde@cluster0.wte6vl5.mongodb.net/tuiter?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/tuiter';
+//const CONNECTION_STRING = 'mongodb+srv://mengruwangcolab:12345abcde@cluster0.wte6vl5.mongodb.net/tuiter?retryWrites=true&w=majority' || 'mongodb://127.0.0.1:27017/tuiter';
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECTION_STRING, { useNewUrlParser: true })
   .then(() => console.log('Connected to MongoDB database!'))
   .catch(err => console.error('Error connecting to MongoDB database:', err));
